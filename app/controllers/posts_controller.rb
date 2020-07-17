b72b11f302b58f789c8a6e9b44b4392b042fc44a
Parker Williamson <parker.williamson@gmail.com>
2020-07-17 00:53:35 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,8 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post = Post.find(params[:id])
-
-    @post.update(post_params)
+    if @post = Post.find(params[:id])
+      @post.update(post_params)
 
     redirect_to post_path(@post)
   end
