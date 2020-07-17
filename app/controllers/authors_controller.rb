8a14c958c96d0b524e2bc926c6ec4fba10434c2d
Parker Williamson <parker.williamson@gmail.com>
2020-07-17 01:12:37 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -8,27 +8,27 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create(author_params)
-      if @author.valid? 
-         @author.save 
-         redirect_to author_path(@author)
-        else
-          render :new 
-      end
+    @author = Author.new(author_params)
+
+    if @author.save
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
   end
-  
-  def edit 
+
+  def edit
     @author = Author.find(params[:id])
   end
-  
-  def update 
+
+  def update
     @author = Author.find(params[:id])
-     
-     if @author.update(author_params)
-        redirect_to author_path(@author)
-      else 
-        render :edit 
-      end
+
+    if @author.update(author_params)
+      redirect_to author_path(@author)
+    else
+      render :edit
+    end
   end
 
   private
